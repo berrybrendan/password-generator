3773d43ef615e30dee579848d750dfce1e5cf23d
Brendan Berry <53982094+Pickingberries@users.noreply.github.com>
2019-10-10 18:36:24 -0700

    Password Generator works with a single input now

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-var lowerCase = confirm("Would you like to include lowercase letters?")
-var upperCase = confirm("Would you like to include uppercase letters?")
-var numericChar = confirm("Would you like to include numeric characters?")
-var specialChar = confirm("Would you like to include special characters?")
-var passwordLength = prompt("How long would you like your password to be? (8-128)")
-var lowerArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var upperArray = lowerArray.map(toUpper)
-var numArray = [1,2,3,4,5,6,7,8,9,0]
-var specialString = "\\ !#$%&'()*+,-./:;<=>]?@[^_`{|}~\""
-var specialArray = specialString.split("")
+var lowerCase = confirm("Would you like to include lowercase letters?");
+var upperCase = confirm("Would you like to include uppercase letters?");
+var numericChar = confirm("Would you like to include numeric characters?");
+var specialChar = confirm("Would you like to include special characters?");
+var passwordLength = prompt("How long would you like your password to be? (8-128)");
+var lowerString = 'abcdefghijklmnopqrstuvwxyz';
+var upperString = lowerString.toUpperCase();
+var numString = '1234567890';
+var specialString = "\\ !#$%&'()*+,-./:;<=>]?@[^_`{|}~\"";
+var specialArray = specialString.split("");
+var passwordEl = document.getElementById('password')
 
-var generateBtn = document.querySelector('#generate')
+var generateBtn = document.querySelector('#generate');
 
-function toUpper(x){
-    return x.toUpperCase();
-}
 
-function randomPassword(length,array){
+function randomPassword(length, string){
     var pass = "";
     for (var z = 0; z < length; z++) {
-        var i = Math.floor(Math.random() * array.length);
-        pass += array.charAt(i);
+        var i = Math.floor(Math.random() * string.length);
+        pass += string.charAt(i);
     }
     return pass;
 }
@@ -28,7 +26,11 @@ function randomPassword(length,array){
 
 
 if (lowerCase && !upperCase && !numericChar && !specialChar) {
-    generateBtn.addEventListener("click",randomPassword(passwordLength,lowerArray))
+    var lowerPass = randomPassword(passwordLength, lowerString);
+    passwordEl.textContent = lowerPass;
+
+    generateBtn.addEventListener("click",randomPassword(passwordLength, lowerString))
+
     
 }/*
 else if (lowerCase && upperCase && !numericChar && !specialChar) {
@@ -47,7 +49,10 @@ else if (lowerCase && !upperCase && numericChar && !specialChar) {
     generateBtn.addEventListener("click",randomPassword(passwordLength,lowerArray))
 }*/
 else if (!lowerCase && upperCase && !numericChar && !specialChar) {
-    generateBtn.addEventListener("click",randomPassword(passwordLength,upperArray))
+    var upperPass = randomPassword(passwordLength, upperString);
+    passwordEl.textContent = upperPass;
+
+    generateBtn.addEventListener("click",randomPassword(passwordLength, upperString))
 }/*
 else if (!lowerCase && upperCase && numericChar && !specialChar) {
     generateBtn.addEventListener("click",randomPassword(passwordLength,lowerArray))
@@ -59,10 +64,16 @@ else if (!lowerCase && upperCase && numericChar && specialChar) {
     generateBtn.addEventListener("click",randomPassword(passwordLength,lowerArray))
 }*/
 else if (!lowerCase && !upperCase && numericChar && !specialChar) {
-    generateBtn.addEventListener("click",randomPassword(passwordLength,numArray))
+    var numPass = randomPassword(passwordLength, numString);
+    passwordEl.textContent = numPass;
+
+    generateBtn.addEventListener("click",randomPassword(passwordLength, numString))
 }
 else if (!lowerCase && !upperCase && !numericChar && specialChar) {
-    generateBtn.addEventListener("click",randomPassword(passwordLength,specialArray))
+    var specialPass = randomPassword(passwordLength, specialString);
+    passwordEl.textContent = specialPass;
+
+    generateBtn.addEventListener("click",randomPassword(passwordLength, specialString))
 }
 /*else if (lowerCase && !upperCase && numericChar && specialChar) {
     generateBtn.addEventListener("click",randomPassword(passwordLength,lowerArray))
